991d90a0601a114429b954acc8f84989906f001f
testmango-sudo <mangotest1965@yahoo.com>
2025-03-01 10:39:45 +0000

    Modified readme file and tested local env files Seeded local database and tested code to insert deatils to database for USERS, ARTICLES and TOPICS. Work still to be done on Comments

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,102 @@
 const db = require("../connection")
+const format = require("pg-format");
+const { convertTimestampToDate, articleLookUp } = require("./utils");
+const { articleData } = require("../data/test-data");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
-};
+        return db.query('DROP TABLE IF EXISTS comments;')
+        .then(() => {
+          return db.query('DROP TABLE IF EXISTS articles;');
+        })
+        .then(() => {
+          return db.query('DROP TABLE IF EXISTS users;');
+        })
+        .then(() => {
+          return db.query('DROP TABLE IF EXISTS topics;');
+        })
+        .then(() => {
+          return createTopics(topicData);
+        })
+        .then(() => {
+          return createUsers(userData);
+        })
+        .then(() => {
+          return createArticles(articleData, topicData);
+        })
+        .then(() => {
+          return createComments(commentData, userData, articleData);
+        })
+}
+
+function createUsers(userData) {
+  return db.query(`CREATE TABLE users (
+    username VARCHAR PRIMARY KEY NOT NULL,
+    name VARCHAR NOT NULL,
+    avatar_url VARCHAR(1000))`).then(() => {
+      const user_list = userData.map((user) => {
+        return [user.username, user.name, user.avatar_url]
+      });
+      const formatted_userlist = format(
+        `INSERT INTO users (username, name, avatar_url) VALUES %L RETURNING *`, user_list)
+      return db.query(formatted_userlist)
+      
+    })
+  
+
+}
+
+function createTopics(topicData) {
+  return db.query(`CREATE TABLE topics(
+    slug VARCHAR(20) PRIMARY KEY NOT NULL,
+    description VARCHAR,
+    img_url VARCHAR(1000)
+    )`).then(() => {
+    const topic_list = topicData.map((topic) => {
+      return [topic.slug, topic.desscription, topic.img_url]
+    });
+    const formatted_topicList = format(
+      `INSERT INTO topics (slug, description, img_url) VALUES %L RETURNING *`, topic_list)
+    return db.query(formatted_topicList);
+  })
+}
+
+function createArticles(articleData, userData, topicData) {
+  return db.query(`CREATE TABLE articles(
+    article_id SERIAL PRIMARY KEY,
+    title VARCHAR,
+    topic VARCHAR REFERENCES topics(slug),
+    author VARCHAR REFERENCES users(username),
+    body TEXT,
+    created_at TIMESTAMP,
+    votes INT DEFAULT 0,
+    article_img_url VARCHAR(1000))`).then((title, topic, author, body, created_at, votes, article_img_url) => { 
+      const article_list = articleData.map((article) => {
+        article_new = convertTimestampToDate(article)
+        return [article.title, article.topic, article.author, article.body, article_new.created_at, article.votes, article.article_img_url]
+      })
+      const formattedArticle_List = format(`INSERT INTO articles 
+        (title, topic, author, body, created_at, votes, article_img_url) VALUES %L RETURNING *`, article_list)
+      return db.query(formattedArticle_List)
+    });
+}
+
+function createComments(commentData, userData, articleData) {
+  return db.query(`CREATE TABLE comments(
+    comment_id SERIAL PRIMARY KEY,
+    article_id INT REFERENCES articles(article_id),
+    body TEXT,
+    votes INT DEFAULT 0,
+    author vARCHAR REFERENCES users(username),
+    created_at TIMESTAMP
+    )`)
+    .then((article_id, body, votes, author, created_at) => { 
+      const article_stats = articleLookUp(articleData)
+      console.log(article_stats, "<<<<<< ----- art STATS")
+    //   const comments_list = commentData.map((comment) => {
+    //     added_create = convertTimestampToDate(comment)
+    //     return [comment.article_id, body, votes, author, added_create.created_at]
+    // //   })
+});
+}
+
 module.exports = seed;
--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -1,9 +1,19 @@
 const db = require("../../db/connection");
+const { commentData } = require("../data/test-data");
 
 exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
   if (!created_at) return { ...otherProperties };
   return { created_at: new Date(created_at), ...otherProperties };
 };
+
+exports.articleLookUp = (articleData) => {
+  const article_fetch= {};
+  articleData.forEach(({ article_id, title }) => {
+    article_fetch[title] = article_id;
+  })
+    return article_fetch
+}
+
 
 
 
