4bf54223e06ad51c22b7c1de7ad91272c6176c85
testmango-sudo <mangotest1965@yahoo.com>
2025-03-16 12:41:59 +0000

    Updated listener,connection file and package.json files

--- listener.js
+++ listener.js
@@ -1,10 +1,15 @@
 const app = require("./app")
 
-app.listen(8080,(err)=>{
-    if(err){
-        console.log(err);
-    }
-    else{
-        console.log("Server Listening on port 8080");
-    }
-});
+// app.listen(9090,(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("Server Listening on port 8080");
+//     }
+// });
+
+const app = require("./app.js");
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
