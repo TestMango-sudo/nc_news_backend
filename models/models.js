4b29ffd427580e8435de45c44d28797f0f986ee1
testmango-sudo <mangotest1965@yahoo.com>
2025-04-02 14:47:03 +0100

    Modified model for get article by id

--- models/models.js
+++ models/models.js
@@ -48,7 +48,7 @@ exports.fetchArticleById = (article_id) => {
     // return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id]).then((data) => {
     //     return data.rows
     //     })
-    return db.query(`SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id)::INT AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`, [article_id]).then((data) => { 
+    return db.query(`SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, article.body, articles.article_img_url, COUNT(comments.article_id)::INT AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`, [article_id]).then((data) => { 
         return data.rows
     })
 }
